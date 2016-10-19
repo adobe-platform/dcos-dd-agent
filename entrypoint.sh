ba7b6a13af21389772ffe2407abce0c6147e340e
Matt Fuller <matthewdf10@gmail.com>
2016-10-19 15:40:23 -0400

    modifying agent to be less aws-specific and to support dcos components

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,38 @@ fi
 
 #################################### ETHOS LOGIC ####################################
 
+# Environment Variables:
+# API_KEY
+# DD_TIER
+# HOST_IP
+# REGION_SHORTNAME
+# MESOS_HOST
+# MESOS_PROTOCOL
+# RDS_FD_INSTANCE
+# RDS_FD_USERNAME
+# RDS_FD_PASSWORD
+# PROXY
+# CAPCOM_PORT
+# STACK_NAME
+# MARATHON_USERNAME
+# MARATHON_PASSWORD
+# ZK_USERNAME
+# ZK_PASSWORD
+# PROJECT_ID
+# TAGS
+# LOG_LEVEL
+
+# Ensure STACK_NAME and PROJECT_ID is set
+if [[ -z $STACK_NAME ]]; then
+    echo "You must set STACK_NAME environment variable to run the Datadog Agent container"
+    exit 1
+fi
+
+if [[ -z $PROJECT_ID ]]; then
+    echo "You must set PROJECT_ID environment variable to run the Datadog Agent container"
+    exit 1
+fi
+
 # Determine current Ethos role:
 if [[ -z $DD_TIER ]]; then
     echo "DD_TIER environment variable not provided. Obtaining via Mesos attributes..."
@@ -28,7 +60,8 @@ echo "Using DD_TIER: $DD_TIER"
 # Determine host IP
 if [[ -z $HOST_IP ]]; then
     echo "HOST_IP environment variable not provided. Obtaining..."
-    HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+    #HOST_IP=$(/sbin/ip route|awk '/default/ { print $9 }')
+    HOST_IP=$(/bin/cat /etc/hostname)
 
     if [[ -z $HOST_IP ]]; then
         echo "Could not determine the host IP address."
@@ -38,16 +71,36 @@ fi
 
 echo "Using HOST_IP: $HOST_IP"
 
-# checks
-if [[ -z $MESOS_HOST ]]; then
-    echo "MESOS_HOST environment variable not provided..."
-    exit 1
+# Determine region shortname
+if [[ -z $REGION_SHORTNAME ]]; then
+    echo "REGION_SHORTNAME environment variable not provided. Defaulting to na..."
+    REGION_SHORTNAME="na"
 fi
+
+echo "Using REGION_SHORTNAME: $REGION_SHORTNAME"
+
+# Determine Capcom Port
+if [[ -z $CAPCOM_PORT ]]; then
+    echo "CAPCOM_PORT environment variable not provided. Defaulting to 2002..."
+    CAPCOM_PORT="2002"
+fi
+
+echo "Using CAPCOM_PORT: $CAPCOM_PORT"
+
+# Determine the Mesos Host
+if [[ -z $MESOS_HOST ]]; then
+    echo "MESOS_HOST environment variable not provided, defaulting to leader.mesos..."
+    MESOS_HOST="leader.mesos"
+fi
+
+echo "Using MESOS_HOST: $MESOS_HOST"
 
 if [[ -z $MESOS_PROTOCOL ]]; then
     echo "MESOS_PROTOCOL environment variable not provided, defaulting to http..."
     MESOS_PROTOCOL="http"
 fi
+
+echo "Using MESOS_PROTOCOL: $MESOS_PROTOCOL"
 
 # copy the relevant tier checks
 echo "Copying checks for tier"
@@ -60,21 +113,22 @@ cp -R /checks.d/shared/* /etc/dd-agent/checks.d/
 if [[ $DD_TIER == "control" ]]; then
     echo "Configuring control tier"
 
-    if [[ $RDS_FD_INSTANCE ]]; then
+    if [[ $RDS_FD_INSTANCE && $RDS_FD_USERNAME && $RDS_FD_PASSWORD ]]; then
         sed -i -e "s/RDS_FD_INSTANCE/${RDS_FD_INSTANCE}/" /etc/dd-agent/conf.d/mysql.yaml
         sed -i -e "s/RDS_FD_USERNAME/${RDS_FD_USERNAME}/" /etc/dd-agent/conf.d/mysql.yaml
         sed -i -e "s/RDS_FD_PASSWORD/${RDS_FD_PASSWORD}/" /etc/dd-agent/conf.d/mysql.yaml
-        
-        if [[ $STACK_NAME ]]; then
-            REGION_SHORT_STRING=`echo $STACK_NAME | awk -F"-" '{print $3}'`
-            sed -i -e "s/REGION_SHORT_STRING/-${REGION_SHORT_STRING}/" /etc/dd-agent/conf.d/mysql.yaml
-        fi
+        sed -i -e "s/REGION_SHORTNAME/-${REGION_SHORTNAME}/" /etc/dd-agent/conf.d/mysql.yaml
+    else
+        # No MySQL DB to monitor
+        echo "" > /etc/dd-agent/conf.d/mysql.yaml
     fi
 
 elif [[ $DD_TIER == "worker" ]]; then
     echo "Configuring worker tier"
 
 elif [[ $DD_TIER == "proxy" ]]; then
+    echo "Configuring proxy tier"
+
     if [[ $PROXY ]]; then
         rm -Rf /etc/dd-agent/conf.d/sw/
         cp /conf.d/${DD_TIER}/sw/${PROXY}.yaml /etc/dd-agent/conf.d/.
@@ -84,13 +138,7 @@ elif [[ $DD_TIER == "proxy" ]]; then
     fi
 
     sed -i -e "s/CAPCOM_HOST/${HOST_IP}/" /etc/dd-agent/conf.d/custom_http.yaml
-
-    if [[ $CAPCOM_PORT ]]; then
-        sed -i -e "s/CAPCOM_PORT/${CAPCOM_PORT}/" /etc/dd-agent/conf.d/custom_http.yaml
-    else
-        echo "No CAPCOM_PORT environment variable detected."
-        exit 1
-    fi
+    sed -i -e "s/CAPCOM_PORT/${CAPCOM_PORT}/" /etc/dd-agent/conf.d/custom_http.yaml
 else
     echo "Invalid DD tier value."
     exit 1
@@ -103,6 +151,11 @@ sed -i -e "s/MESOS_PROTOCOL/${MESOS_PROTOCOL}/" /etc/dd-agent/conf.d/shared_http
 sed -i -e "s/MESOS_PROTOCOL/${MESOS_PROTOCOL}/" /etc/dd-agent/conf.d/custom_http.yaml
 sed -i -e "s/HOST_IP/${HOST_IP}/" /etc/dd-agent/conf.d/custom_http.yaml
 
+sed -i -e "s/STACK_NAME/${STACK_NAME}/" /etc/dd-agent/conf.d/shared_http.yaml
+sed -i -e "s/STACK_NAME/${STACK_NAME}/" /etc/dd-agent/conf.d/custom_http.yaml
+sed -i -e "s/PROJECT_ID/${PROJECT_ID}/" /etc/dd-agent/conf.d/shared_http.yaml
+sed -i -e "s/PROJECT_ID/${PROJECT_ID}/" /etc/dd-agent/conf.d/custom_http.yaml
+
 if [[ $MARATHON_USERNAME && $MARATHON_PASSWORD ]]; then
     sed -i -e "s/MARATHON_AUTH/${MARATHON_USERNAME}:${MARATHON_PASSWORD}@/" /etc/dd-agent/conf.d/shared_http.yaml
 else
@@ -113,14 +166,6 @@ if [[ $ZK_USERNAME && $ZK_PASSWORD ]]; then
     sed -i -e "s/ZK_AUTH/${ZK_USERNAME}:${ZK_PASSWORD}@/" /etc/dd-agent/conf.d/shared_http.yaml
 else
     sed -i -e "s/ZK_AUTH//" /etc/dd-agent/conf.d/shared_http.yaml
-fi
-
-if [[ $STACK_NAME ]]; then
-    sed -i -e "s/STACK_NAME/${STACK_NAME}/" /etc/dd-agent/conf.d/shared_http.yaml
-    sed -i -e "s/STACK_NAME/${STACK_NAME}/" /etc/dd-agent/conf.d/custom_http.yaml
-    PROJECT_ID=`echo $STACK_NAME | cut -d - -f 1`-`echo $STACK_NAME | cut -d - -f 2`-`echo $STACK_NAME | cut -d - -f 3`
-    sed -i -e "s/PROJECT_ID/${PROJECT_ID}/" /etc/dd-agent/conf.d/shared_http.yaml
-    sed -i -e "s/PROJECT_ID/${PROJECT_ID}/" /etc/dd-agent/conf.d/custom_http.yaml
 fi
 
 #####################################################################################
